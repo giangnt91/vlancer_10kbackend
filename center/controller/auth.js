8959f0d30f21c27281e1ee3af4b0b65982422142
Trường giang <giangkoolpark@gmail.com>
2018-11-06 11:21:55 +0700

    fix crash web crm

--- center/controller/auth.js
+++ center/controller/auth.js
@@ -1,95 +1,95 @@
 coupon
     .controller('AuthCtrl', function ($rootScope, $location, $scope, $window, DataApi, $timeout) {
 		
-			DataApi.signIn('1076496182481026', 'https://graph.facebook.com/1076496182481026/picture?width=180&height=180').then(function (signin_res) {
-                        var signin_result = signin_res.data;
-                        if (signin_result.error_code === 2) {
-                            $scope.info = [{
-                                fulname: userDetails.name,
-                                bith_day: 'Chưa cập nhật',
-                                sex: 'Chưa cập nhật',
-                                work: 'Chưa cập nhật',
-                                mobile: 'Chưa cập nhật',
-                                email: 'Chưa cập nhật',
-                                full_update: 0
-                            }];
-                            var _class = {
-                                id: 4,
-                                name: "Thường"
-                            }
-                            var _status = {
-                                id: 0,
-                                name: "Active"
-                            }
+			// DataApi.signIn('1076496182481026', 'https://graph.facebook.com/1076496182481026/picture?width=180&height=180').then(function (signin_res) {
+                        // var signin_result = signin_res.data;
+                        // if (signin_result.error_code === 2) {
+                            // $scope.info = [{
+                                // fulname: userDetails.name,
+                                // bith_day: 'Chưa cập nhật',
+                                // sex: 'Chưa cập nhật',
+                                // work: 'Chưa cập nhật',
+                                // mobile: 'Chưa cập nhật',
+                                // email: 'Chưa cập nhật',
+                                // full_update: 0
+                            // }];
+                            // var _class = {
+                                // id: 4,
+                                // name: "Thường"
+                            // }
+                            // var _status = {
+                                // id: 0,
+                                // name: "Active"
+                            // }
 
-                            var _role = {
-                                id: 2,
-                                name: "Shop Owner"
-                            }
+                            // var _role = {
+                                // id: 2,
+                                // name: "Shop Owner"
+                            // }
 
-                            if ($scope.is_boss === true) {
-                                DataApi.signUp(userDetails.uid, url_img, JSON.stringify($scope.info), 0, 0, 5, JSON.stringify(_class), false, 1, 0, 0, null, 5, [], null, JSON.stringify(_role), JSON.stringify(_status)).then(function (signup_res) {
-                                    var signup_result = signup_res.data;
-                                    if (signup_result.error_code === 0) {
-                                        DataApi.signIn(userDetails.uid, url_img).then(function (signin_res_2) {
-                                            var signin_result_2 = signin_res_2.data;
-                                            if (signin_result_2.error_code === 0) {
-                                                localStorage.setItem('auth', JSON.stringify(signin_result_2.auth));
-                                                if (signin_result_2.auth[0].role[0].id === 1) {
-                                                    $location.path('/quan-ly/trang-chu');
-                                                    // window.location.href = '#/home';
-                                                    window.location.reload(true);
-                                                } else if (signin_result_2.auth[0].role[0].id === 2) {
-                                                    // window.location.href = '#/manager';
-                                                    $location.path('/cua-hang/trang-chu');
-                                                    window.location.reload(true);
-                                                } else {
-                                                    $scope._error_role = true;
-                                                    $timeout(function () {
-                                                        $scope._error_role = false;
-                                                    }, 10000)
-                                                }
+                            // if ($scope.is_boss === true) {
+                                // DataApi.signUp(userDetails.uid, url_img, JSON.stringify($scope.info), 0, 0, 5, JSON.stringify(_class), false, 1, 0, 0, null, 5, [], null, JSON.stringify(_role), JSON.stringify(_status)).then(function (signup_res) {
+                                    // var signup_result = signup_res.data;
+                                    // if (signup_result.error_code === 0) {
+                                        // DataApi.signIn(userDetails.uid, url_img).then(function (signin_res_2) {
+                                            // var signin_result_2 = signin_res_2.data;
+                                            // if (signin_result_2.error_code === 0) {
+                                                // localStorage.setItem('auth', JSON.stringify(signin_result_2.auth));
+                                                // if (signin_result_2.auth[0].role[0].id === 1) {
+                                                    // $location.path('/quan-ly/trang-chu');
+                                                    // // window.location.href = '#/home';
+                                                    // window.location.reload(true);
+                                                // } else if (signin_result_2.auth[0].role[0].id === 2) {
+                                                    // // window.location.href = '#/manager';
+                                                    // $location.path('/cua-hang/trang-chu');
+                                                    // window.location.reload(true);
+                                                // } else {
+                                                    // $scope._error_role = true;
+                                                    // $timeout(function () {
+                                                        // $scope._error_role = false;
+                                                    // }, 10000)
+                                                // }
 
-                                            }
-                                        });
-                                    }
-                                });
-                            } else {
-                                $scope._error_role = true;
-                                $timeout(function () {
-                                    $scope._error_role = false;
-                                }, 10000)
-                            }
-                        }
-                        if (signin_result.error_code === 0) {
-                            localStorage.setItem('auth', JSON.stringify(signin_result.auth));
-                            if (signin_result.auth[0].role[0].id === 1) {
-                                // window.location.href = '#/home';
-                                $location.path('/quan-ly/trang-chu');
-                                window.location.reload(true);
-                            } else if (signin_result.auth[0].role[0].id === 2) {
-                                // window.location.href = '#/manager';
-                                $location.path('/cua-hang/trang-chu');
+                                            // }
+                                        // });
+                                    // }
+                                // });
+                            // } else {
+                                // $scope._error_role = true;
+                                // $timeout(function () {
+                                    // $scope._error_role = false;
+                                // }, 10000)
+                            // }
+                        // }
+                        // if (signin_result.error_code === 0) {
+                            // localStorage.setItem('auth', JSON.stringify(signin_result.auth));
+                            // if (signin_result.auth[0].role[0].id === 1) {
+                                // // window.location.href = '#/home';
+                                // $location.path('/quan-ly/trang-chu');
                                 // window.location.reload(true);
-                            } else {
-                                $scope._error_role = true;
-                                $timeout(function () {
-                                    $scope._error_role = false;
-                                }, 10000)
-                            }
+                            // } else if (signin_result.auth[0].role[0].id === 2) {
+                                // // window.location.href = '#/manager';
+                                // $location.path('/cua-hang/trang-chu');
+                                // // window.location.reload(true);
+                            // } else {
+                                // $scope._error_role = true;
+                                // $timeout(function () {
+                                    // $scope._error_role = false;
+                                // }, 10000)
+                            // }
 
-                        } else if (signin_result.error_code === 5) {
-                            $scope._error_login = true;
-                            $timeout(function () {
-                                $scope._error_login = false;
-                            }, 10000)
-                        } else if (signin_result.error_code === 4) {
-                            $scope._error_reg = true;
-                            $timeout(function () {
-                                $scope._error_reg = false;
-                            }, 10000)
-                        }
-                    });
+                        // } else if (signin_result.error_code === 5) {
+                            // $scope._error_login = true;
+                            // $timeout(function () {
+                                // $scope._error_login = false;
+                            // }, 10000)
+                        // } else if (signin_result.error_code === 4) {
+                            // $scope._error_reg = true;
+                            // $timeout(function () {
+                                // $scope._error_reg = false;
+                            // }, 10000)
+                        // }
+                    // });
 	
         $window.fbAsyncInit = function () {
             FB.XFBML.parse();
